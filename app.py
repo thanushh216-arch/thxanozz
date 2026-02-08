f8cdc6a3a7529197ae872136129bcb4399bb7537
thanushh216-arch <thanushh216@gmail.com>
2026-02-09 01:50:07 +0530

    Add model download logic

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+import os
+import requests
 import numpy as np
 import tensorflow as tf
+MODEL_URL = "https://drive.google.com/uc?id=1e8aOjV6A9B1ZMakxfcdfSTrq9mj4gglh"
+MODEL_PATH = "model.pkl"
+
+def download_model():
+    if not os.path.exists(MODEL_PATH):
+        print("Downloading model...")
+        r = requests.get(MODEL_URL)
+        with open(MODEL_PATH, "wb") as f:
+            f.write(r.content)
+        print("Model downloaded")
+
+download_model()
+
 
 app = FastAPI()
 
